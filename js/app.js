173fcb596f5947d7b9ef83796969b5c2a70be551
yangfeng_ZR <yangfeng12345@139.com>
2019-08-21 09:43:09 +0800

    feat(20190821):vue测试回顾

---
+++ js/app.js
@@ -0,0 +1,36 @@
+/**
+ * el：element 是html根元素
+ * data:数据存储
+ * methods:用于存储各种方法
+ *
+ */
+new Vue({
+	el:'#vue-app',
+	data:{
+		message:"hello world",
+		name:"Ms Yang",
+		job:"web开发",
+		website:"http://www.thenewstep.com",
+		websiteTag:'<a href="http://www.thenewstep.com">web网站</a>',
+		age:30,
+		x:0,
+		y:0
+	},
+	methods:{
+		greet:function(time){
+			//代码块
+			return 'good '+time+" "+this.name+"!"
+		},
+		add:function(inc){
+			this.age +=inc;
+		},
+		substract:function(desc){
+			this.age -=desc;
+		},
+		updateXY:function(event){
+			console.log(event);
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		}
+	}
+})
